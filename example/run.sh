834e709444594f3307f7f4a3d8d30a74426f84b3
winkidney <winkidney@gmail.com>
2015-07-23 17:00:49 +0800

    add worker option

--- example/run.sh
+++ example/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gunicorn -b :9000 -k flask_sockets.worker flask_ws:app --debug  --log-level info
+gunicorn -b :9000 -k flask_sockets.worker flask_ws:app --debug  --log-level info --worker-connections 2000 --workers 8
